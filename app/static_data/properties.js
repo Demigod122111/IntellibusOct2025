ace856d444ab579e865be96802b40cd6774d9fe9
Demigod122111 <simonsmithcampion9@gmail.com>
2025-10-19 07:25:59 -0500

    7:25 AM

--- app/static_data/properties.js
+++ app/static_data/properties.js
@@ -1,8 +1,8 @@
 export const Properties = {
     "AppName": "AgriDex",
     "AppDescription": "",
-    "SupportEmail": "support@agridex.com",
-    "SupportContact": "+1 (876) 555-0198"
+    "SupportEmail": "intellibusogteamoct@gmail.com",
+    "SupportContact": "+1 (876) 874-2546"
 }
 
 Properties.AppDescription = `${Properties.AppName} bridges the gap between Jamaica’s farmers and consumers —
